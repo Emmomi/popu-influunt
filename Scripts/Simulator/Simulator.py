9fea72061f29c5e99ade599d79ea587b3eea727f
Emmomi <kgrowth12@gmail.com>
2022-06-09 14:57:08 +0900

    add indecade people system

--- Scripts/Simulator/Simulator.py
+++ Scripts/Simulator/Simulator.py
@@ -4,6 +4,7 @@ class room:
     def __init__(self,x,y):
         self.current_people=x
         self.limit_people=y
+        return None
     def leave(self,x):
         self.current_people=self.current_people-x
         return self.current_people
@@ -11,35 +12,50 @@ class room:
         self.current_people=self.current_people+x
         return self.current_people
     def check_exceed(self):
-        return self.current_people>self.limit_people
-    def show_room(self,a):
-        if a==1:
-            print("current:{} limit:{}\n".format(self.current_people,self.limit_people))
+        return ((self.current_people)>(self.limit_people))
+    def show_room(self):
+        print("current:{} limit:{}\n".format(self.current_people,self.limit_people))
+        return 0
+    def people(self,mood=None):
+        if mood=='c':
+            return self.current_people
+        elif mood=='l':
+            return self.limit_people
+        elif mood!=None:
+            return None
         return self.current_people,self.limit_people
 
 
 class Simulator:
-    def __init__(self):
-        j_data=open('rooms.json','r')
-        self.rooms_data=json.load(j_data)
+    def __init__(self,path):
+        with open(path,'r') as j_data:
+            self.rooms_data=json.load(j_data)
         self.Rooms=[]
-        for i in range(1,self.rooms_data["number"]):
+        for i in range(1,self.rooms_data["number"]+1):
             self.Rooms.append(room(self.rooms_data["room"+str(i)]["current_people"],self.rooms_data["room"+str(i)]["limit_people"]))
+            #print(self.Rooms)
         return None
     def transfer(self,x_r,x_p,y_r):
         self.Rooms[x_r].leave(x_p)
         self.Rooms[y_r].enter(x_p)
         return 0
     def Show(self):
-        for i in range(1,len(self.Rooms)):
-            self.Rooms[i].show_room(1)
+        print("population of room")
+        for i in range(0,len(self.Rooms)):
+            print("room {}".format(i+1))
+            self.Rooms[i].show_room()
         return 0
+    def people(self,x,mood=None):
+        if mood=='e':
+            return self.Rooms[x].check_exceed
+        return self.Rooms[x].people(mood)
 
 
     
 
 if __name__ == "__main__":
-    Simu=Simulator()
-    #Simu.Show()
+    Simu=Simulator('rooms.json')
+    Simu.Show()
     Simu.transfer(0,20,1)
     Simu.Show()
+    print(Simu.people(1,'e'))
