50703432c00434637d61633282a14ed971b9c2df
Emmomi <kgrowth12@gmail.com>
2022-08-02 17:08:41 +0900

    add Q functions

--- Scripts/Agent/Agent.py
+++ Scripts/Agent/Agent.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.append(os.pardir)
-from env import popu
+#from env import popu
 import tensorflow as tf
 import numpy as np
 
@@ -18,8 +18,18 @@ class DQNAgent:
         self.exploration=0.1
         self.D=deque(maxlen=self.episode)
 
+        self.init_model()
+
         self.current_loss=0.0
+    def init_model(self):
+        
+
+    def Q_values(self, state):
+        # Q(state, action) of all actions
+        return self.sess.run(self.y, feed_dict={self.x: [state]})[0]
+
     def select_action(self,state,ep):
         if np.random.rand()<=ep:
-            return np.random.randint(len(state)),np.random.randint(len(state)),np.random.rand()
+            return np.random.randint(len(state)),np.random.randint(len(state)),np.random.rand()  #移動元、移動先、人数の割合
         else:
+            return 
